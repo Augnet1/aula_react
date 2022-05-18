14c034b0fd58867d9e7c390ba0150cd88afeedbf
Augusto <augusto.silva@univasf.edu.br>
2022-05-18 19:48:56 -0300

    Lesson 01

--- lesson-01/src/index.js
+++ lesson-01/src/index.js
@@ -2,11 +2,23 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles.css"
 
+function sum(a, b) {
+    return a + b
+}
+
+function primeiroJSX() {
+    return (
+        <div className="teste">
+            Augusto César - Introdução ao ReactJS
+            <h1> Soma: {sum(10, 20)}</h1>
+        </div>
+    )
+}
 const App = () => {
 
     return (
         <div className="App">
-            Hello World!
+            Hello World!!! {primeiroJSX()}
         </div>
     )
 }
