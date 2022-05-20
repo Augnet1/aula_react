a99b924aa2c927480b9601d28774ed8ee946a2e0
Augusto <augusto.silva@univasf.edu.br>
2022-05-20 13:44:46 -0300

    Lesson 05

---
+++ lesson-05/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+    return (
+        <div>
+            <p>Teste Webpack!</p>
+            <p>Bem vindo.</p>
+            <div>Webpack 4!</div>
+            <Button />
+        </div>
+    );
+};
+
+export default App;
---
+++ lesson-05/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
